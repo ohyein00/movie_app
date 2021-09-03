9c3412f075c752d91f6d46408ea225b144e12bb2
yein <yein@spadecompany.kr>
2021-09-03 16:56:23 +0900

    dl,dt연습

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="info_frame">
+      <div class="frame_head">
+        <p>한 패턴으로 시공할 경우</p>
+      </div>
+      <div class="frame_body">
+        <dl>
+          <dt>천장 시공 면적</dt>
+          <dd><span class="left">1,000,000,000,000,000,000 <b class="unit">Roll</b></span><span class="right">1,333m</span></dd>
+        </dl>
+        <dl>
+          <dt>벽 시공</dt>
+          <dd><span>74 <b className="unit">㎡</b></span><span>1,333m</span></dd>
+        </dl>
+        <dl>
+          <dt>천장 제외한 벽지</dt>
+          <dd><span>74 <b className="unit">㎡</b></span></dd>
+        </dl>
+        <dl>
+          <dt>천장 시공 면적</dt>
+          <dd><span>74 <b className="unit">Roll</b></span><span>930,303m</span></dd>
+        </dl>
+      </div>
     </div>
   );
 }
